3f74ba98b70d8f161c002b8916f15ae3088a0283
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-03-29 10:50:41 -0400

    Write code to upload to Cloudinary

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Canvas } from 'skia-canvas';
+import { cloudinary } from './cloudinary.config';
 
 // Most social media platforms have and image ratio of 1.91:1.
 // Facebook recommends 1200x630, X/Twitter 800x418, LinkedIn 1200x627.
@@ -10,6 +11,15 @@ const ctx = canvas.getContext('2d');
 
 async function render() {
 	// "Save" the canvas to a Node Buffer.
-	// Convert the Buffer to a readable stream.
-	// Upload the stream to Cloudinary
+	const buffer = await canvas.toBuffer('png');
+	// Upload the buffer to Cloudinary.
+	cloudinary.uploader
+		.upload_stream({}, (err, res) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(res);
+		})
+		.end(buffer);
 }
