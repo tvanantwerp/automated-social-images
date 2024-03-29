cda47cff332ec7110f73facafa62bbca4cf776b8
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-03-29 15:12:21 -0400

    Factor out functions and add a background image

--- index.ts
+++ index.ts
@@ -1,25 +1,17 @@
-import { Canvas } from 'skia-canvas';
-import { cloudinary } from './cloudinary.config';
+import {
+	createSoicalImage,
+	saveImageToFile,
+	uploadToCloudinary,
+} from './social-sharing';
 
-// Most social media platforms have and image ratio of 1.91:1.
-// Facebook recommends 1200x630, X/Twitter 800x418, LinkedIn 1200x627.
-// Facebook's recommendation will work well for all platforms, so we'll use that.
-// These dimensions were chosen in March, 2024. They may change in the future.
-const canvas = new Canvas(1200, 630);
-const { width, height } = canvas;
-const ctx = canvas.getContext('2d');
+const postNames = [
+	'Leveraging TypeScript for Scalable Web Applications: Strategies and Best Practices',
+	'Next-Gen Web Performance Optimization: Techniques for 2024',
+	'Building Responsive Interfaces with React Hooks: A Comprehensive Guide',
+];
 
-async function render() {
-	// "Save" the canvas to a Node Buffer.
-	const buffer = await canvas.toBuffer('png');
-	// Upload the buffer to Cloudinary.
-	cloudinary.uploader
-		.upload_stream({}, (err, res) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			console.log(res);
-		})
-		.end(buffer);
+for (const post of postNames) {
+	const canvas = createSoicalImage(post);
+	saveImageToFile(post, canvas);
+	// uploadToCloudinary(canvas);
 }
---
+++ social-sharing.ts
@@ -0,0 +1,45 @@
+import { Canvas, loadImage, ExportFormat } from 'skia-canvas';
+import { cloudinary } from './cloudinary.config';
+
+// We're using skia-canvas to create an image instead of native canvas.
+// This is to make it easier to wrap text, which native canvas doesn't
+// support out-of-the-box.
+
+export async function createSoicalImage(name: string): Canvas {
+	// Most social media platforms have and image ratio of 1.91:1.
+	// Facebook recommends 1200x630, X/Twitter 800x418, LinkedIn 1200x627.
+	// Facebook's recommendation will work well for all platforms, so we'll use that.
+	// These dimensions were chosen in March, 2024. They may change in the future.
+	const canvas = new Canvas(1200, 630);
+	const { width, height } = canvas;
+	const ctx = canvas.getContext('2d');
+
+	// Draw the background image.
+	const image = await loadImage('./background.png');
+	ctx.drawImage(image, 0, 0, width, height);
+
+	return canvas;
+}
+
+export async function saveImageToFile(
+	name: string,
+	canvas: Canvas,
+	format?: ExportFormat,
+) {
+	await canvas.saveAs(name, { format });
+}
+
+export async function uploadToCloudinary(canvas: Canvas) {
+	// "Save" the canvas to a Node Buffer.
+	const buffer = await canvas.toBuffer('png');
+	// Upload the buffer to Cloudinary.
+	cloudinary.uploader
+		.upload_stream({}, (err, res) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			console.log(res);
+		})
+		.end(buffer);
+}
