78bfa10ecf567f07d15cf6f37afb93a995002ac0
Tom VanAntwerp <tom@tomvanantwerp.com>
2024-03-29 15:30:54 -0400

    Add Inter font

--- social-sharing.ts
+++ social-sharing.ts
@@ -1,11 +1,11 @@
-import { Canvas, loadImage, ExportFormat } from 'skia-canvas';
+import { Canvas, loadImage, ExportFormat, FontLibrary } from 'skia-canvas';
 import { cloudinary } from './cloudinary.config';
 
 // We're using skia-canvas to create an image instead of native canvas.
 // This is to make it easier to wrap text, which native canvas doesn't
 // support out-of-the-box.
 
-export async function createSoicalImage(name: string): Canvas {
+export async function createSoicalImage(name: string): Promise<Canvas> {
 	// Most social media platforms have and image ratio of 1.91:1.
 	// Facebook recommends 1200x630, X/Twitter 800x418, LinkedIn 1200x627.
 	// Facebook's recommendation will work well for all platforms, so we'll use that.
@@ -15,8 +15,12 @@ export async function createSoicalImage(name: string): Canvas {
 	const ctx = canvas.getContext('2d');
 
 	// Draw the background image.
-	const image = await loadImage('./background.png');
+	const image = await loadImage('./assets/background.png');
 	ctx.drawImage(image, 0, 0, width, height);
+
+	// Load the Inter font, weight 600, available on Google Fonts.
+	FontLibrary.use(['./assets/Inter-SemiBold.ttf']);
+	ctx.font = '48px Inter';
 
 	return canvas;
 }
